a48a052e7159e1b1c0d15a0af335ff64cfa618d7
SteamedKoko <SteamedKoko@gmail.com>
2025-06-30 07:59:08 +0900

    feat: food follows player, fix: food sprites swapping

--- entities/monsters/base_monster.gd
+++ entities/monsters/base_monster.gd
@@ -3,10 +3,22 @@ extends Area2D
 
 @export var monster_sprite = GlobalSpriteAssets.MonsterSprites.PIE
 
-@onready var anim: SpriteLoader = $SpriteLoader
+@onready var player = get_parent().get_node("Player")
+
+
+var anim: SpriteLoader
+var veclocity: float = 100
 
 func _ready() -> void:
 	var res = GlobalSpriteAssets.MONSTER_RESOURCES.get(monster_sprite).resource
 	print(res)
+	anim = SpriteLoader.new()
+	self.add_child(anim)
 	anim.load_sprite(res, 35, 35, 4, 1) 
+
 	anim.play()
+
+func _physics_process(_delta: float) -> void:
+	if(player):
+		self.position = self.position.move_toward(player.position, _delta * veclocity)
+	
--- utils/sprite_loader/sprite_loader.gd
+++ utils/sprite_loader/sprite_loader.gd
@@ -1,7 +1,11 @@
 class_name SpriteLoader
 extends AnimatedSprite2D
 
+func _init() -> void:
+	pass
+
 func load_sprite(asset: Resource,width: int, height: int, columns: int, row_to_use: int, animation_name: String = "default") -> void:
+	sprite_frames = SpriteFrames.new()
 	for i in range(columns):
 		var atlas = AtlasTexture.new()
 		atlas.atlas = asset
